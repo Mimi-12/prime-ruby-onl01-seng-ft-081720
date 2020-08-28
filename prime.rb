f0cca3905bdcdd5c8aa6781fb663a7367fff43ce
selam lemma <oneiconforlife26@gmail.com>
2020-08-28 16:52:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(number)
+    start = 2
+    primes = (start..number).to_a
+    (start..number).each do |no|
+      (start..no).each do |num|
+        if ( no % num  == 0) && num != no
+        return true
+        braek
+        end
+      end
+    end
+  end
